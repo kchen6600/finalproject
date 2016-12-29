655dca0db74a95a9cb586a891d212cf11ed648db
Joyce Wu <jwu26@stuy.edu>
2016-12-29 11:17:18 -0500

    slight outline of sidebar

--- Sidebar/Sidebar.java
+++ Sidebar/Sidebar.java
@@ -1,2 +1,39 @@
-public class Sidebar{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Sidebar extends JFrame implements ActionListener{
+    private Container sidebar;
+    private Jlabel title;
+
+    public Sidebar(){
+	this.setTitle("Your Glorified Post-it Notes");
+	this.setSize(1000, 600);
+	this.setLocation(0, 0);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	sidebar = this.getContentPane();
+	sidebar.getLayout(new BoxLayout(sidebar, BoxLayout.Y_AXIS));
+
+	JButton newNote = new JButton("New Note");
+	newNote.addActionListener(this);
+	b.setActionCommand("newNote");
+
+	//parse through file names and list all titles and time stamps
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals("newNote")){
+	    //create new note method
+	}
+    }
+
+    
+}
+	
+	
+	    
+	    
+
 }
