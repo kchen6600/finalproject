a062b92f5c913cb1717891aeaf085be3ad072f75
Joyce Wu <jwu26@stuy.edu>
2017-01-06 23:04:54 -0500

    beginning changes for delete

--- Sidebar.java
+++ Sidebar.java
@@ -24,8 +24,6 @@ public class Sidebar extends JFrame implements ActionListener{
 	newNote.setActionCommand("newNote");
 	
 
-	//how will delete button work? do more research one double click/one
-	//click
 	
 	JButton delete = new JButton("Delete Note");
 	delete.addActionListener(this);
@@ -34,14 +32,6 @@ public class Sidebar extends JFrame implements ActionListener{
 	sidebar.add(newNote);
 	sidebar.add(delete);
 
-	/**
-	//create directory for post it notes
-	File directoryName = new File("postitnotes");
-	if(!directoryName.exists()){
- 	    boolean success = directoryName.mkdir();
-
-	}
-	**/
        
 	//parse through file names and list all titles
 	File dir = new File("postitnotes/");
@@ -64,32 +54,94 @@ public class Sidebar extends JFrame implements ActionListener{
 	
 
 	//how are timestamps organized in files??
-	
 
+    }
 
-
-    }	    
 
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
 	if(event.equals("newNote")){
-	    //Postitnotes postit = new Postitnotes();
+	    Postitnotes postit = new Postitnotes();
 
        	}
 	if(event.substring(0, 3).equals("txt")){
 	    
-	    //where will openNote method be located?
-	    
-	    //postit.openNote(event.substring(3));
+	    Postitnotes() post = new Postitnotes();
 	}
 
 	if(event.equals("delete")){
-	    //how does delete work?
+	    SidebarDelete deleteNotes = new SidebarDelete();
 	}
     }
 
+    public class SidebarDelete extends JFrame implements ActionListener{
+	private Container sidebarDelete;
+	private JLabel title;
+
+	public SidebarDelete(){
+	    this.setTitle("Your Glorified Post-it Notes");
+	    this.setSize(500, 1000);
+	    this.setLocation(10, 10);
+	    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+	    sidebarDelete = this.getContentPane();
+	    sidebarDelete.setLayout(new BoxLayout(sidebar, BoxLayout.Y_AXIS));
+
+	    JButton newNote = new JButton("New Note");
+	    newNote.addActionListener(this);
+	    newNote.setActionCommand("newNote");
+       
+	
+	    JButton delete = new JButton("Delete Note");
+	    delete.addActionListener(this);
+	    delete.setActionCommand("delete");
+
+	    JButton deleteSelected = new JButton("Delete Selected");
+	    deleteSelected.addActionListener(this);
+	    deleteSelected.setActionCommand("deleteSelected");
+
+	    JButton cancel = new JButton("Cancel");
+	    cancel.addActionListener(this);
+	    cancel.setActionCommand("cancel");
+
+	    sidebarDelete.add(newNote);
+	    sidebarDelete.add(delete);
+
+       
+	    //parse through file names and list all titles
+	    File dir = new File("postitnotes/");
+	    File[] directoryListing = dir.listFiles();
+	    if(directoryListing.length != 0){
+		for(File child : directoryListing){
+		    System.out.println(child.getName());
+		    String name = child.getName();
+		    int i = name.indexOf(".txt");
+		    String fileName = name.substring(0,i);
+		    /**
+		    JButton fileOnSidebar = new JButton(fileName);
+		    fileOnSidebar.addActionListener(this);
+		    fileOnSidebar.setActionCommand("txt" + fileName);
+		    sidebar.add(fileOnSidebar);
+		    **/
+}
+	    }
+	    else{
+		System.out.println("File don't work");
+	    }
+	
+
+	    //how are timestamps organized in files??
+
+	    sidebarDelete.add(deleteSelected);
+	    sidebarDelete.add(cancel);
+
+	}
+    }
+    
     public static void main(String[] args){
 	Sidebar hi = new Sidebar();
 	hi.setVisible(true);
     }
+
 }
