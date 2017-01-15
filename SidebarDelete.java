c123f33ad22eb5795e01499c6c2f79584a22bd3c
Joyce Wu <jwu26@stuy.edu>
2017-01-15 14:46:11 -0500

    format sidebarDelete gui

--- SidebarDelete.java
+++ SidebarDelete.java
@@ -19,17 +19,34 @@ public class SidebarDelete extends JFrame implements ActionListener{
 	sidebarDelete.setLayout(new BoxLayout(sidebarDelete, BoxLayout.Y_AXIS));
 
 	this.setTitle("Your Glorified Post-it Notes-Deleting");
-	this.setSize(500, 1000);
+	this.setSize(300, 1000);
 	this.setLocation(10, 10);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+	JButton instruct = new JButton("To delete multiple files, hold shift.");
+	instruct.setFont(new Font("Verdana", Font.PLAIN, 15));
+	instruct.setFocusPainted(false);
+	instruct.setMaximumSize(new Dimension(300, 50));
+	instruct.setAlignmentX(Component.CENTER_ALIGNMENT);
+	instruct.setBackground(Color.GRAY);
+	instruct.setBorderPainted(false);
+	    
+	
 	JButton deleteSelected = new JButton("Delete Selected");
 	deleteSelected.addActionListener(this);
 	deleteSelected.setActionCommand("deleteSelected");
+	deleteSelected.setFont(new Font("Verdana", Font.PLAIN, 15));
+	deleteSelected.setFocusPainted(false);
+	deleteSelected.setMaximumSize(new Dimension(140, 35));
+	deleteSelected.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 	JButton cancel = new JButton("Cancel");
 	cancel.addActionListener(this);
 	cancel.setActionCommand("cancel");
+	cancel.setFont(new Font("Verdana", Font.PLAIN, 18));
+	cancel.setFocusPainted(false);
+	cancel.setMaximumSize(new Dimension(140, 35));
+	cancel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 	//parse through file names and list all titles
 	File dir = new File("postitnotes/");
@@ -53,13 +70,23 @@ public class SidebarDelete extends JFrame implements ActionListener{
 	}
 
 	JList<String> list = new JList<>(fileList);
+	list.setMinimumSize(new Dimension (300,800));
+	list.setFixedCellWidth(250);
+	list.setFixedCellHeight(20);
+	DefaultListCellRenderer renderer =  (DefaultListCellRenderer)list.getCellRenderer();  
+	renderer.setHorizontalAlignment(JLabel.CENTER); 
 	list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 	list.addListSelectionListener(new ListSelectionListener(){
 		public void valueChanged(ListSelectionEvent event){
 		    filesDeleted.add(fileList[list.getSelectedIndex()]);
 		}
 	    });
+	list.setFont(new Font("Verdana", Font.PLAIN, 15));
+
+	sidebarDelete.add(Box.createRigidArea(new Dimension(20,20)));
+	sidebarDelete.add(instruct);
 	sidebarDelete.add(list);
+	sidebarDelete.add(Box.createRigidArea(new Dimension(20,20)));
 	sidebarDelete.add(deleteSelected);
 	sidebarDelete.add(cancel);
 	setVisible(true);
